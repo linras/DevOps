ecd3d539669562f677c759f906110b8ab52752a2
Antonina Brzeska <linras@wp.pl>
2021-05-13 21:57:32 +0200

    Docker compose and improved backend api.

--- mybackend/index.js
+++ mybackend/index.js
@@ -10,11 +10,11 @@ const redisClient = redis.createClient({
     port: 6379,
 }),
 pgCLient = new Pool({
-    user: "",
-    password: "",
-    database: "",
-    host: "",
-    port: ""
+    user: "postgres",
+    password: "Pa$$w0rd",
+    database: "postgres",
+    host: "mypostgres",
+    port: "5432"
 });
 
 app.use(cors());
@@ -25,12 +25,66 @@ pgCLient.on('error', () => console.log("Postgres not connected"));
 pgCLient.query('CREATE TABLE IF NOT EXISTS dogs (id INT, name VARCHAR(20))')
     .catch((err) => console.log(err));
 
-app.get("/", (req, res) => {
+redisClient.on('connect', () => console.log("Connected to redis sever"));
+
+const getDogs= (_, res) => {
+    pgClient.query('SELECT * FROM dogs ORDER BY name ASC', (error, results) => {
+        if (error) {
+            throw error
+        }
+        res.status(200).json(results.rows)
+    })
+}
+
+const getDog = (req, res) => {
+    const id = parseInt(req.params.id)
+
+    redisClient.get(id, (_, results) => {
+        if (results) {
+            let result = [{'id': id, 'name': results}]
+            res.status(200).json(result)
+            console.log(`Retrieved from cache`)
+        } else {
+            pgClient.query('SELECT * FROM dogs WHERE id = $1', [id], (error, results) => {
+                if (error) {
+                    throw error
+                }
+                res.status(200).json(results.rows)
+            })
+        }
+    })
+}
+
+const newDog= (req, res) => {
+    const { name } = req.body
+
+    const dbQuery = new Promise((resolve, _) => {
+        pgClient.query('INSERT INTO dogs (name) VALUES ($1) RETURNING id', [name], (error, results) => {
+            if (error) {
+                throw error
+            }
+            id = results.rows[0].id
+            res.status(201).send(`Dog ${name} was added`)
+            resolve(id)
+        })
+    })
+    dbQuery.then(value =>
+        redisClient.set(value, name, (error, _) => {
+            if (error) {
+                res.status(500).json({ error: error })
+                console.log(error)
+            }
+        })
+    )
+}
+
+app.get("/", (_, res) => {
     res.status(200).send("Hello dogs, wow!");
 });
 
-redisClient.on('connect', () => console.log("Connected to redis sever"));
-
+app.get("/dogs", getDogs)
+app.get("/dogs/:id", getDog)
+app.post("/dogs", newDog)
 
 app.listen(port, () => {
     console.log(`Look for dogs on http://localhost:${port}`);
